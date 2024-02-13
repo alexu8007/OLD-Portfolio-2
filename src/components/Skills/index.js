cbc1b2d8cca7b8870ef1493130795f05cf46d1dd
Alex <107227218+alexu8007@users.noreply.github.com>
2024-02-13 17:22:12 -0500

    Update index.js

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -163,7 +163,7 @@ const Skills = () => {
     <Container id="skills">
       <Wrapper>
         <Title className="FadeInLeft">Skills</Title>
-        <Desc className="FadeInLeft">Here are some of my skills on which I have been working on for the past 2 years.
+        <Desc className="FadeInLeft">Here are some of my skills on which I have been working on for the past 4 years.
         </Desc>
         <SkillsContainer>
           {skills.map((skill) => (
